d2e8d79444eeaddc79273b869600cf4d88bb4650
tmdqls2257 <tmdqls2257@naver.com>
2021-07-01 00:13:00 +0900

    click to top arrow

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 'use strict';
+
+const arrowBtn = document.querySelector('.home__arrow');
+
 // Make navbar transparent blue
 const navbar = document.querySelector('#navbar');
 document.addEventListener('scroll', () => {
   if(window.scrollY > 500){
     navbar.classList.add('navbar--blue');
+    arrowBtn.classList.add('visable');
   }else{
     navbar.classList.remove('navbar--blue');
+    arrowBtn.classList.remove('visable');
   }
 });
+
 
 // !select navbar menu to move!
 const navbarMenu = document.querySelector(".navbar__menu");
@@ -32,3 +38,18 @@ function scrollIntoView(selector){
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// arrow that go to top
+
+  arrowBtn.addEventListener('click', () => {
+    scrollIntoView('#Home');
+  });
+
+// My work
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector(`.work__projects`);
+
+workBtnContainer.addEventListener('click', (event) => {
+  const filter = event.target.dataset.fillter || event.target.parentNode.dataset.filter;
+  console.log(filter);
+});
