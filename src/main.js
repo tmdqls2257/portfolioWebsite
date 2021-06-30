0bc109a34644c7778691ea51c135d346bc8306ca
tmdqls2257 <tmdqls2257@naver.com>
2021-06-30 16:31:21 +0900

    home contact btn

--- src/main.js
+++ src/main.js
@@ -2,10 +2,33 @@
 // Make navbar transparent blue
 const navbar = document.querySelector('#navbar');
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
   if(window.scrollY > 500){
     navbar.classList.add('navbar--blue');
   }else{
     navbar.classList.remove('navbar--blue');
   }
 });
+
+// !select navbar menu to move!
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener(`click`, (event) => {
+  const link = event.target.dataset.link;
+  if (link == null) {
+    return;
+  } else {
+    scrollIntoView(link);
+  }
+});
+
+// Handle click on "contact me"
+const homeContact = document.querySelector(".home__contact");
+
+homeContact.addEventListener(`click`, (event) => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
